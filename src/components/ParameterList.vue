ef5eb4481518d393a21d79c925ef7537442d7392
Five Grant <5@fivegrant.com>
2023-01-18 15:39:21 -0600

    Add useless button

--- src/components/ParameterList.vue
+++ src/components/ParameterList.vue
@@ -1,11 +1,17 @@
 <template>
 	<div>
         <DataTable :value="parameters" responsiveLayout="scroll">
-            <Column field="name" header="Name"></Column>
-            <Column field="defaultValue" header="Default"></Column>
-            <Column field="type" header="Type"></Column>
-            <Column field="curie" header="CURIE"></Column>
-            <Column field="stateVariable" header="State Variable"></Column>
+            <Column field="name" header="Name"/>
+            <Column field="defaultValue" header="Default"/>
+            <Column field="type" header="Type"/>
+            <Column field="curie" header="CURIE"/>
+            <Column field="stateVariable" header="State Variable"/>
+						<Column headerStyle="width: 1rem; text-align: center" bodyStyle="text-align: center; overflow: visible">
+             <template #body>
+              <Button type="button" icon="pi pi-times" 
+							        class="p-button-outlined p-button-danger p-button-rounded"/>
+             </template>
+            </Column>
         </DataTable>
 	</div>
 </template>
@@ -14,6 +20,7 @@
 import { ref, computed, defineComponent } from 'vue';
 import DataTable from 'primevue/datatable';
 import Column from 'primevue/column';
+import Button from 'primevue/button';
 
 const props = defineProps(["parameters"]);
 
