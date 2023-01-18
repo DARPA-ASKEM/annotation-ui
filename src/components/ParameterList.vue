0a3c843ed796d7f0dc2857fc7bb1ab12e5db7e6d
Five Grant <5@fivegrant.com>
2023-01-18 13:33:22 -0600

    Add Parameter LIst (#11)
    * Use DataTable
    
    * Add some cleanup

--- src/components/ParameterList.vue
+++ src/components/ParameterList.vue
@@ -1,11 +1,27 @@
 <template>
-<h3>Parameter List</h3>
+	<div>
+        <DataTable v-model="parameters" responsiveLayout="scroll">
+            <Column field="name" header="Name"></Column>
+            <Column field="defaultValue" header="Default"></Column>
+            <Column field="type" header="Type"></Column>
+            <Column field="curie" header="CURIE"></Column>
+            <Column field="stateVariable" header="State Variable"></Column>
+        </DataTable>
+	</div>
 </template>
 
 <script setup>
 import { ref, computed, defineComponent } from 'vue';
 import DataTable from 'primevue/datatable';
 import Column from 'primevue/column';
+
+const props = defineProps(["parameters"]);
+
+const parameters = computed({
+        get: () => props.visibleMenu,
+        set: sidebarValue => emit('update:parameters', sidebarValue),
+});
+
 
 </script>
 
--- src/components/ParameterMenu.vue
+++ src/components/ParameterMenu.vue
@@ -1,8 +1,8 @@
 <template>
 
 <Sidebar 
-     v-model:visible="openMenu"
-     position=left
+     v-model:visible="visibleMenu"
+     position=right
 >
 <div>
         <h3>Annotating parameter:</h3>
@@ -55,22 +55,25 @@ const typeOptions = ref([
   {name: 'int'}, {name:"string"},{name:"boolean"},{name:"float"}
 ]);
 
-const openMenu = computed({
-        get: () => props.visibleMenu,
-        set: sidebarValue => emit('update:visibleMenu', sidebarValue),
-});
-
-const formData = ref({
+const formDefaults = {
         "name": "",
         "type": "",
         "curie": "",
         "defaultValue": props.selectedValue,
         "stateVariable": true,
+};
+
+const visibleMenu = computed({
+        get: () => props.visibleMenu,
+        set: sidebarValue => emit('update:visibleMenu', sidebarValue),
 });
+
+const formData = ref({...formDefaults});
 
 const submit = () => {
   props.appendParameter(formData.value);
   emit('update:visibleMenu', false);
+  formData.value = {...formDefaults};
 };
 
 </script>
