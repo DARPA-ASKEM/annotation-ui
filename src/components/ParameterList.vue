b8387cec2d74d70f3799b659ade59a1bea763260
Five Grant <5@fivegrant.com>
2023-01-18 16:08:59 -0600

    Add parameter list buttons (#18)
    * Add useless button
    
    * Add useless button

--- src/components/ParameterList.vue
+++ src/components/ParameterList.vue
@@ -1,11 +1,22 @@
 <template>
 	<div>
         <DataTable :value="parameters" responsiveLayout="scroll">
-            <Column field="name" header="Name"></Column>
-            <Column field="defaultValue" header="Default"></Column>
-            <Column field="type" header="Type"></Column>
-            <Column field="curie" header="CURIE"></Column>
-            <Column field="stateVariable" header="State Variable"></Column>
+				    <template #header>
+              <div style="text-align: left">
+                    <Button icon="pi pi-external-link" label="Save to Data Service" @click="submit" />
+              </div>
+            </template>
+            <Column field="name" header="Name"/>
+            <Column field="defaultValue" header="Default"/>
+            <Column field="type" header="Type"/>
+            <Column field="curie" header="CURIE"/>
+            <Column field="stateVariable" header="State Variable"/>
+						<Column headerStyle="width: 1rem; text-align: center" bodyStyle="text-align: center; overflow: visible">
+             <template #body="row">
+              <Button type="button" icon="pi pi-times" @click="removeParam(row)"
+							        class="p-button-outlined p-button-danger p-button-rounded"/>
+             </template>
+            </Column>
         </DataTable>
 	</div>
 </template>
@@ -14,8 +25,12 @@
 import { ref, computed, defineComponent } from 'vue';
 import DataTable from 'primevue/datatable';
 import Column from 'primevue/column';
+import Button from 'primevue/button';
 
 const props = defineProps(["parameters"]);
+
+const removeParam = ({ index }) => parameters.value.splice(index, 1);
+const submit = () => {};
 
 const parameters = computed({
         get: () => props.parameters,
