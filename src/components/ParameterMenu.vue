1be5171f1397495f277496ea414879b03c918bc3
Five Grant <5@fivegrant.com>
2023-01-18 11:00:02 -0600

    Include type in payload

--- src/components/ParameterMenu.vue
+++ src/components/ParameterMenu.vue
@@ -15,7 +15,7 @@
         <InputText id="param-value" class="spacer" 
                    type="text" v-model="formData.defaultValue" />
 
-        <Dropdown class="spacer matchInput" v-model="formData.selectedType" 
+        <Dropdown class="spacer matchInput" v-model="formData.type" 
                   :options="typeOptions" optionLabel="name" optionValue="name" 
                   placeholder="Select Type" />
 
@@ -41,7 +41,7 @@
 </template>
 
 <script setup>
-import { ref, computed, defineComponent } from 'vue';
+import { ref, computed } from 'vue';
 import Sidebar from 'primevue/sidebar';
 import InputText from 'primevue/inputtext';
 import Button from 'primevue/button'
@@ -50,6 +50,16 @@ import Dropdown from 'primevue/dropdown'
 
 const emit = defineEmits(['update:visibleMenu']);
 const props = defineProps(["appendParameter", "visibleMenu", "selectedValue"]);
+
+const typeOptions = ref([
+  {name: 'int'}, {name:"string"},{name:"boolean"},{name:"float"}
+]);
+
+const openMenu = computed({
+        get: () => props.visibleMenu,
+        set: sidebarValue => emit('update:visibleMenu', sidebarValue),
+});
+
 const formData = ref({
         "name": "",
         "type": "",
@@ -57,13 +67,7 @@ const formData = ref({
         "defaultValue": props.selectedValue,
         "stateVariable": true,
 });
-const typeOptions = ref([
-  {name: 'int'}, {name:"string"},{name:"boolean"},{name:"float"}
-]);
-const openMenu = computed({
-        get: () => props.visibleMenu,
-        set: sidebarValue => emit('update:visibleMenu', sidebarValue),
-});
+
 const submit = () => {
   props.appendParameter(formData.value);
   emit('update:visibleMenu', false);
